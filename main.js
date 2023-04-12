d5102323adadc5508f25c456e9a435cf5c70c6f6
SebasGT08 <guaman.sebastian802@gmail.com>
2023-04-12 10:54:19 -0500

    Segundo Commit

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+function showMenu() {
+    // Obtener el elemento del menú desplegable
+    
+    var submenu = document.getElementById("submenu");
+    
+    // Cambiar la clase CSS del elemento para mostrar u ocultar el menú desplegable
+    submenu.classList.toggle("show");
+    console.log(submenu);
+}
